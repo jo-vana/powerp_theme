b49ff991f12b256c1fb7dcaf91667fe82063b3a7
jo-vana <jovananukic85@gmail.com>
2022-05-19 18:25:54 +0200

    Home page-in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 //All your JS code goes here
+jQuery(function() {
+    var header = $(".header");
+  
+    jQuery(window).scroll(function() {    
+        var scroll = jQuery(window).scrollTop();
+        if (scroll >= 130) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+  
+});
