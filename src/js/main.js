2bd55e71def46149cacc570f43a4b7dbaed9fc1e
jo-vana <jovananukic85@gmail.com>
2022-05-20 17:00:42 +0200

    Responsive

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ jQuery(function() {
   
     jQuery(window).scroll(function() {    
         var scroll = jQuery(window).scrollTop();
-        if (scroll >= 130) {
+        if (scroll >= 50) {
             header.addClass("scrolled");
         } else {
             header.removeClass("scrolled");
@@ -27,3 +27,23 @@ btn.on('click', function(e) {
   e.preventDefault();
   jQuery('html, body').animate({scrollTop:0}, '300');
 });
+// Partners section-slider
+jQuery('.owl-carousel').owlCarousel({
+  margin:30,
+  nav:false,
+  items: 3,
+  loop:true,
+  autoPlay:true,
+  autoplayTimeout:2000,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1200:{
+          items:2
+      }
+  }
+})
